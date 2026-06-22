aa47fa4373d868f646fbb862980bcfb24670c1d8
the-legend-sinmayan <sinmayankokul693@gmail.com>
2026-06-22 17:24:05 +0200

    commit file

---
+++ h/rita.sql
@@ -0,0 +1,38 @@
+
+CREATE TABLE IF NOT EXISTS DEPARTMENT (
+    EMPLOYEE_ID TEXT,
+    NAME TEXT,
+    DEPARTMENT_ID TEXT,
+    MANAGER_ID TEXT,
+    SALARY REAL
+);
+
+
+INSERT INTO DEPARTMENT (EMPLOYEE_ID, NAME, DEPARTMENT_ID, MANAGER_ID, SALARY) VALUES
+('100', 'STEVEN KING', '90', '100', 24000),
+('101', 'NEENA KOCHCAR', '90', '100', 17000),
+('102', 'LEX DEHAAN', '90', '102', 9000),
+('103', 'BRUCE LEE', '60', '103', 4800),
+('104', 'DIANA WILLS', '60', '103', 25000),
+('105', 'VALLI PATOR', '50', '100', 4200),
+('1973', 'LUV HAMI', '60', '102', 5000),
+('106', 'DAVID AUSTIN', '90', '100', 6000);
+
+SELECT 
+    SUM(SALARY) AS total_salary,
+    AVG(SALARY) AS average_salary,
+    COUNT(DISTINCT DEPARTMENT_ID) AS department_count,
+    MIN(SALARY) AS min_salary,
+    MAX(SALARY) AS max_salary
+FROM DEPARTMENT;
+
+SELECT 
+    DEPARTMENT_ID AS 'Department Code',
+    COUNT(*) AS 'No of Employees',
+    SUM(SALARY) AS 'Total Salary',
+    AVG(SALARY) AS 'Average Salary',
+    MIN(SALARY) AS 'Min Salary',
+    MAX(SALARY) AS 'Max Salary'
+FROM DEPARTMENT
+GROUP BY DEPARTMENT_ID;
+
